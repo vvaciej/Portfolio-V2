ed311406b8073d65870c560d6dec9eb5ce745e9f
Maciek Skokowski <maciejskok2004@gmail.com>
2024-06-30 22:10:46 +0200

    added navbar and 2 first blocks

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,81 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<div className='bg-[#1c1c1e] h-[calc(100vh+65rem)] w-full text-black'>
+			<div className='w-full h-full font-[system-ui] flex items-center flex-col font-semibold text-[#eee]'>
+				<header className='h-40 w-full relative flex items-center justify-center'>
+					<div className='w-[70rem] z-10 relative flex justify-between items-center'>
+						<span className='text-[#ffffffc4]'>&copy; 2024 maciejskok</span>
+						<nav className='h-max py-1 mr-10 bg-[#111112] px-[.3rem] rounded-full flex gap-1'>
+							<button className='h-full w-full bg-[#2C2C2E] px-4 py-1 rounded-full'>Wszystko</button>
+							<button className='h-full w-full hover:brightness-50 transition-all px-4 py-1 rounded-full'>
+								O mnie
+							</button>
+							<button className='h-full w-full hover:brightness-50 transition-all px-4 py-1 rounded-full'>
+								Projekty
+							</button>
+						</nav>
+						<a
+							href='mailto:maciejskok@proton.me'
+							className='border-4 border-blue-500 py-1 px-4 rounded-full bg-gradient-to-r from-blue-300 to-blue-500 text-white cursor-pointer hover:scale-105 transition-all'>
+							Kontakt
+						</a>
+					</div>
+				</header>
+				<div className='w-full h-9 bg-[#63e6be] text-[#080808] flex items-center justify-center'>
+					<h2>Aktualnie jestem dostępny</h2>
+				</div>
+				<main className='w-[70rem] mt-16 h-full grid grid-cols-4 grid-rows-6 gap-5 mb-32'>
+					<div className='w-full h-full bg-[#2C2C2E] col-span-2 rounded-[2rem] py-3 px-4 pb-1 flex items-center justify-center flex-col'>
+						<h1 className='text-5xl font-extrabold bg-gradient-to-r from-blue-300 to-blue-500 bg-clip-text text-transparent select-none'>
+							Maciek Skokowski
+						</h1>
+						<p className='mt-1 text-[1.32rem] select-none'>Student, samouk front-end developer w Warszawie</p>
+						<p className='font-normal text-center mt-3 select-none'>
+							Hej! Jestem Maciek i moim celem jest praca w firmie której będe dostarczał wartość biznesową i w trakcie
+							której moje umięjetności będą się podwyższały jako developer.
+						</p>
+						<section className='mt-4 flex gap-4'>
+							<button className='border-4 border-blue-500 py-1 px-4 rounded-full bg-gradient-to-r from-blue-300 to-blue-500 text-white cursor-pointer hover:scale-105 transition-all'>
+								Moje prace
+							</button>
+							<button className='bg-[#1C1C1E] rounded-full px-4 hover:scale-105 transition-transform'>
+								Więcej o mnie
+							</button>
+						</section>
+					</div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem] relative overflow-hidden'>
+						<img
+							className='object-cover absolute top-[-4rem] scale-[121.5%] left-0'
+							src='https://i.ibb.co/2qBks6M/448974962-1521669402065793-421239235051550492-n.jpg'
+							alt=''
+						/>
+					</div>
+					<div className='w-full h-full bg-[#2C2C2E] row-span-2 rounded-[2rem] px-4 flex items-center flex-col justify-center py-8'>
+						<h2 className='text-3xl font-bold'>Co wykorzystuje</h2>
+						<section className='mt-4'></section>
+					</div>
+					<div className='w-full h-full bg-[#2C2C2E] row-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] col-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] col-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] col-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] row-span-2 rounded-[2rem] p-8'>
+						<span className='uppercase text-[.8rem] tracking-wide text-gray-400'>Projekt</span>
+						<h3 className='text-[1.6rem] font-bold leading-6 mb-4'>Psychoterapeuta</h3>
+						<span className='uppercase text-[.8rem] text-gray-400 tracking-wide'>Built with</span>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+					</div>
+					<div className='w-full h-full bg-[#2C2C2E] row-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] col-span-2 rounded-[2rem]'></div>
+					<div className='w-full h-full bg-[#2C2C2E] rounded-[2rem]'></div>
+				</main>
+			</div>
+		</div>
+	);
 }
