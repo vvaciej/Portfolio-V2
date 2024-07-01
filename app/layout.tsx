552fa1da514062b81fc656195dcd1bfe89892473
Maciek Skokowski <maciejskok2004@gmail.com>
2024-07-01 17:22:50 +0200

    added analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
 import { Analytics } from '@vercel/analytics/react';
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
 import '../styles/css/home.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pl_PL">
-      <body className={inter.className}>{children}</body>
-      <Analytics />
-    </html>
-  );
+	return (
+		<>
+			<html lang='pl_PL'>
+				<body className={inter.className}>{children}</body>
+			</html>
+			<Analytics />
+		</>
+	);
 }
